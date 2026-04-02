487d8e9d167400a228fb5b7aced627b18c83167f
Cheam Norakpanha <cheam.panha168@gmail.com>
2026-04-02 13:44:38 +0700

    project setup

---
+++ src/main/java/org/me/pvh_group_02_spring_mini_project/PvhGroup02SpringMiniProjectApplication.java
@@ -0,0 +1,13 @@
+package org.me.pvh_group_02_spring_mini_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PvhGroup02SpringMiniProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PvhGroup02SpringMiniProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/me/pvh_group_02_spring_mini_project/PvhGroup02SpringMiniProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.me.pvh_group_02_spring_mini_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PvhGroup02SpringMiniProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
