459e5a8c1335d96a2447b425a2cb11817d9bf306
Cheam Norakpanha <cheam.panha168@gmail.com>
2026-04-04 22:49:20 +0700

    make changes on habit

--- src/main/java/org/me/pvh_group_02_spring_mini_project/model/entity/Habit.java
+++ src/main/java/org/me/pvh_group_02_spring_mini_project/model/entity/Habit.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.me.pvh_group_02_spring_mini_project.model.response.AppUserResponse;
 
 import java.time.Instant;
 import java.util.UUID;
@@ -17,6 +18,7 @@ public class Habit {
     private String description;
     private String frequency;
     private Boolean isActive;
-    private UUID appUserId;
+//    private UUID appUserId;
+    private AppUserResponse appUserResponse;
     private Instant createdAt;
 }
